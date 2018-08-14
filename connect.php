bbd2c2629abe2d91ec3c6bc4146ba90352980849
almina-mac <alm.0554.mina@gmail.com>
2018-08-14 16:57:45 +0900

    Update: clean code

--- connect.php
+++ connect.php
@@ -1,21 +1,21 @@
 <?php
-    /*
-    Program:
-        authentify with database, not using PDO.
-    */
+/*
+Program:
+    authentify with database, not using PDO.
+*/
 
-    $url = parse_url(getenv("DATABASE_URL"));
-    $con = pg_connect("host=" . $url['host']
-                     . " port=" . $url['port']
-                     . " dbname=" . substr($url['path'], 1)
-                     . " user=" . $url['user']
-                     . " password=" . $url['pass']
-                    );
+$url = parse_url(getenv("DATABASE_URL"));
+$con = pg_connect("host=" . $url['host']
+                    . " port=" . $url['port']
+                    . " dbname=" . substr($url['path'], 1)
+                    . " user=" . $url['user']
+                    . " password=" . $url['pass']
+                );
 
-    $res = pg_query($con, "select * from sample") or die("not work" . pg_last_error());
-    while($row = pg_fetch_row($res)){
-        print_r($row);
-    }
+$res = pg_query($con, "select * from sample") or die("not work" . pg_last_error());
+while($row = pg_fetch_row($res)){
+    print_r($row);
+}
 
-    pg_close($con);
+pg_close($con);
 ?>
