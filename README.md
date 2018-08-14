d3419aa33989fcfe4d689f414fc9ca2e96151417
almina-mac <alm.0554.mina@gmail.com>
2018-08-14 12:26:03 +0900

    Update: update database and query

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # simple-SQL-injection
 test for simple SQL injection
+
+## Note
+- シンプルなSQLインジェクションの実験用ログインフォーラム
+- 以下のようなデータベースを想定する
+
+| id | name | age | password |
+| --- | --- | --- | --- |
+| 1 | 山田太郎 | 28 | yamada |
+| 2 | 佐藤隆 | 36 | sato |
+| 3 | 斎藤達弘 | 46 | saito |
+| 4 | 桜井さつき | 22 | sakurai |
--- auth.php
+++ auth.php
@@ -2,6 +2,9 @@
 <?php
 $id = $_POST['user_id'];
 $passwd = $_POST['user_password'];
-$sql = "SELECT COUNT(*) FROM Users WHERE UserID = '$id' AND Password = '$passwd'";  // SQL process
+
+// make query
+// $sql = "SELECT COUNT(*) FROM Users WHERE id = '$id' AND password = '$passwd'";
+$sql = "SELECT * FROM Users WHERE id = '$id' AND password = '$passwd'";
 echo $sql;
 ?>
