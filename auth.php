d805ffd00faacb84b3f1119cb25b3826cc9f14a5
almina-mac <alm.0554.mina@gmail.com>
2018-08-14 18:27:12 +0900

    Update: add output form as function

--- auth.php
+++ auth.php
@@ -16,19 +16,10 @@
 
 <body>
     <h3>Search result</h3>
-    <div class="form" style="margin: 5px">
-        <table>
-            <tr>
-                <td>りんご</td>
-                <td>甘酸っぱい</td>
-                <td>おおむね赤</td>
-            </tr>
-            <tr>
-                <td>なつみかん</td>
-                <td>かなり酸っぱいと思う</td>
-                <td>たいてい黄色</td>
-            </tr>
-        </table>
+    <div class="form" style="margin: 5px; border: solid 1px">
+        <?php
+        output();
+        ?>
     </div>
 </body>
 
@@ -40,35 +31,47 @@ Program:
     authentify with database.
 */
 
-try {
-    /*** Connect by PHP Data Object (PDO) ***/
-    $url = parse_url(getenv('DATABASE_URL'));  // using heroku env
-    $dsn = sprintf('pgsql:host=%s;dbname=%s', $url['host'], substr($url['path'], 1));
-    $dbh = new PDO($dsn, $url['user'], $url['pass']);  // connect using PDO
+function output() {
+    try {
+        /*** Connect by PHP Data Object (PDO) ***/
+        $url = parse_url(getenv('DATABASE_URL'));  // using heroku env
+        $dsn = sprintf('pgsql:host=%s;dbname=%s', $url['host'], substr($url['path'], 1));
+        $dbh = new PDO($dsn, $url['user'], $url['pass']);  // connect using PDO
 
-    /*** Process on database ***/
-    $id = $_POST['user_id'];
-    $passwd = $_POST['user_password'];
+        /*** Process on database ***/
+        $id = $_POST['user_id'];
+        $passwd = $_POST['user_password'];
 
-    $sql = "SELECT * FROM sample WHERE id = '$id' AND password = '$passwd'";  // make query
-    // $sql = "SELECT * FROM sample";
+        $sql = "SELECT * FROM sample WHERE id = '$id' AND password = '$passwd'";  // make query
+        // $sql = "SELECT * FROM sample";
 
-    // output
-    // $stmt = $dbh->query($sql);
-    // while($result = $stmt->fetch(PDO::FETCH_ASSOC)) {
-    //     print($result['id']);
-    //     print($result['name']);
-    //     print($result['age']);
-    //     print($result['password']);
-    // }
-    foreach($dbh->query($sql) as $row) {
-        print_r($row);
+        // output
+        // $stmt = $dbh->query($sql);
+        // while($result = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        //     print($result['id']);
+        //     print($result['name']);
+        //     print($result['age']);
+        //     print($result['password']);
+        // }
+        // foreach($dbh->query($sql) as $row) {
+        //     print_r($row);
+        // }
+        $html = '';
+        foreach($dbh->query($sql) as $data) {
+            $line = '<tr>';
+            $line .= '<td>' . $data['id'] . '</td>';
+            $line .= '<td>' . $data['name'] . '</td>';
+            $line .= '<td>' . $data['age'] . '</td>';
+            $line .= '<td>' . $data['password'] . '</td>';
+            $html .= $line . '</tr>'; // $htmlに1行分追加
+        }
+        $html .= '<table>' .$html. '</table>';
+
+        /*** Disconnect ***/
+        $dbh = null;
+    } catch (PDOException $e) {
+        print("Error: " . $e->getMessage()."<br/>");
+        die();
     }
-
-    /*** Disconnect ***/
-    $dbh = null;
-} catch (PDOException $e) {
-    print("Error: " . $e->getMessage()."<br/>");
-    die();
 }
 ?>
